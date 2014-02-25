bdbf8681c7c9f26ec860c6331c210994d2ea6395
fission6 <fission6@fission6.(none)>
2014-02-24 21:19:14 -0500

    cleaned up with maps and copy

--- js/custom.js
+++ js/custom.js
@@ -26,34 +26,34 @@ jQuery(document).ready(function($) {
 			optionsList += '<option value="' + $anchor.attr('href') + '">' + indent + ' ' + $anchor.text() + '</option>';
 		}).end()
 		.after('<select class="selectmenu">' + optionsList + '</select>');
-		
+
 		$('select.selectmenu').on('change', function() {
 			window.location = $(this).val();
 		});
-		
+
 	})();
 
-	
+
 		  $('.toggle-link').each(function() {
 			$(this).click(function() {
 			  var state = 'open'; //assume target is closed & needs opening
 			  var target = $(this).attr('data-target');
 			  var targetState = $(this).attr('data-target-state');
-			  
+
 			  //allows trigger link to say target is open & should be closed
 			  if (typeof targetState !== 'undefined' && targetState !== false) {
 				state = targetState;
 			  }
-			  
+
 			  if (state == 'undefined') {
 				state = 'open';
 			  }
-			  
+
 			  $(target).toggleClass('toggle-link-'+ state);
-			  $(this).toggleClass(state);      
+			  $(this).toggleClass(state);
 			});
 		  });
-	
+
 		//add some elements with animate effect
 
 		$(".big-cta").hover(
@@ -64,6 +64,7 @@ jQuery(document).ready(function($) {
 			$('.cta a').removeClass("animated shake");
 			}
 		);
+        /*
 		$(".box").hover(
 			function () {
 			$(this).find('.icon').addClass("animated pulse");
@@ -75,36 +76,36 @@ jQuery(document).ready(function($) {
 			$(this).find('.text').removeClass("animated fadeInUp");
 			$(this).find('.image').removeClass("animated fadeInDown");
 			}
-		);
-		
-		
+		);*/
+
+
 		$('.accordion').on('show', function (e) {
-		
+
 			$(e.target).prev('.accordion-heading').find('.accordion-toggle').addClass('active');
 			$(e.target).prev('.accordion-heading').find('.accordion-toggle i').removeClass('icon-plus');
 			$(e.target).prev('.accordion-heading').find('.accordion-toggle i').addClass('icon-minus');
 		});
-		
+
 		$('.accordion').on('hide', function (e) {
 			$(this).find('.accordion-toggle').not($(e.target)).removeClass('active');
 			$(this).find('.accordion-toggle i').not($(e.target)).removeClass('icon-minus');
 			$(this).find('.accordion-toggle i').not($(e.target)).addClass('icon-plus');
-		});	
+		});
 
 
-		
+
 		//Navi hover
 		$('ul.nav li.dropdown').hover(function () {
 			$(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn();
 		}, function () {
 			$(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut();
 		});
-		
+
 		// tooltip
 		$('.social-network li a, .options_box .color a').tooltip();
 
 		// fancybox
-		$(".fancybox").fancybox({				
+		$(".fancybox").fancybox({
 				padding : 0,
 				autoResize: true,
 				beforeShow: function () {
@@ -116,7 +117,7 @@ jQuery(document).ready(function($) {
 				}
 			});
 
-		
+
 		//scroll to top
 		$(window).scroll(function(){
 			if ($(this).scrollTop() > 100) {
@@ -132,8 +133,8 @@ jQuery(document).ready(function($) {
 
 		$('#mycarousel').jcarousel();
 		$('#mycarousel1').jcarousel();
-		
-	
+
+
 		//TWITTER
 		getTwitters('twitter', {
 			id: 'wrapbootstrap',
@@ -144,11 +145,12 @@ jQuery(document).ready(function($) {
 			newwindow: true
 		});
 
-	
+
 		//flexslider
 		$('.flexslider').flexslider();
-	
+
 		//nivo slider
+        /*
 		$('.nivo-slider').nivoSlider({
 			effect: 'random', // Specify sets like: 'fold,fade,sliceDown'
 			slices: 15, // For slice animations
@@ -170,9 +172,9 @@ jQuery(document).ready(function($) {
 			slideshowEnd: function(){}, // Triggers after all slides have been shown
 			lastSlide: function(){}, // Triggers when last slide is shown
 			afterLoad: function(){} // Triggers when slider has loaded
-		});
-				
-		//slitslider				
+		});*/
+
+		//slitslider
 		var Page = (function() {
 
 			var $nav = $( '#nav-dots > span' ),
@@ -190,17 +192,17 @@ jQuery(document).ready(function($) {
 				$nav.each( function( i ) {
 					$( this ).on( 'click', function() {
 					var $dot = $( this );
-			
+
 					if( !slitslider.isActive() ) {
 						$nav.removeClass( 'nav-dot-current' );
 						$dot.addClass( 'nav-dot-current' );
 					}
-										
+
 					slitslider.jump( i + 1 );
 					return false;
-									
+
 					} );
-									
+
 				} );
 
 			};
